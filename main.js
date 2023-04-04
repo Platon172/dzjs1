92ea803e242e6d51aa57a900def2defc753c18cb
Platon172 <107851230+Platon172@users.noreply.github.com>
2023-04-04 20:12:21 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+let car = {
+
+    brand : "mersedes",
+    speed : 250,
+
+
+
+   drive: function() {
+        console.log("Driving");
+    }
+
+
+
+} 
+
+car.drive ();
+
+
+let student = {
+    name : "Platon",
+    age : 12 ,
+
+
+    write : function() {
+        console.log("Writing")
+    }
+}
+
+student.write();
